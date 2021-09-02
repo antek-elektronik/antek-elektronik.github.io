1370bf5def5e250b537a465c1a25706c82ac6f9f
Antoni Gzara <antek.gzara@gmail.com>
2021-09-02 20:44:06 +0200

    updated files

---
+++ navigationButtons.js
@@ -0,0 +1,9 @@
+function Home(){
+  window.location.href = "https:\\\\antek-gzara-animations.github.io\\shop";
+}
+function Apps(){
+  window.location.href = "https:\\\\antek-gzara-animations.github.io\\shop\\programs";
+}
+function Games(){
+  window.location.href = "https:\\\\antek-gzara-animations.github.io\\shop\\games";
+}
