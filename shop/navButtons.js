36a8fff1a95af8036462dbf37d51b8f17b9772d4
Antoni Gzara <antek.gzara@gmail.com>
2021-09-03 16:06:56 +0200

    updated files

--- shop/navButtons.js
+++ shop/navButtons.js
@@ -1,5 +1,6 @@
 function Home(){
-  window.location.href = "https:\\\\antek-gzara-animations.github.io\\shop";
+  //window.location.href = "https:\\\\antek-gzara-animations.github.io\\shop";
+  document.getElementsByClassName("HeaderButton").style.color = "red";
 }
 function Apps(){
   window.location.href = "https:\\\\antek-gzara-animations.github.io\\shop\\programs";
@@ -7,3 +8,12 @@ function Apps(){
 function Games(){
   window.location.href = "https:\\\\antek-gzara-animations.github.io\\shop\\games";
 }
+
+document.getElementById("HomeBtn").style.position = "static";
+document.getElementById("Apps").style.position = "static";
+document.getElementById("Games").style.position = "static";
+
+document.getElementById("HomeBtn").style.display = "inline-block";
+document.getElementById("Apps").style.display = "inline-block";
+document.getElementById("Games").style.display = "inline-block";
+
