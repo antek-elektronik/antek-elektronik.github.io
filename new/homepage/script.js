676923c4d61c968c8761af70ea2116704d30da0f
antek-gzara-animations <58851540+Antek-Gzara-Animations@users.noreply.github.com>
2022-06-21 21:35:12 +0200

    started working on new version of website

--- new/homepage/script.js
+++ new/homepage/script.js
@@ -1,10 +1,21 @@
 //test
 console.log("Welcome to my website!");
 
+var polishCheckbox = document.getElementById("polishCheckbox");
+
+polishCheckbox.checked = true;
+
+document.cookie = "test=hello";
+document.cookie = "language=polish";
+
+console.log(document.cookie);
+
 //update logo size on navbar based on navbar height
 window.setInterval(function(){
     var navbarHeight = document.getElementById("navbar").offsetHeight - 10;
     document.getElementById("logo").style.height = navbarHeight.toString() + "px";
     document.getElementById("logo").style.width = navbarHeight.toString() + "px";
-    //console.warn(navbarHeight)
+    //console.warn(navbarHeight) 
 }, 100);
+
+    
