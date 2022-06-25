9e4e4ef032b04d8941b645417d56df71f8011f27
antek-gzara-animations <58851540+Antek-Gzara-Animations@users.noreply.github.com>
2022-06-25 04:54:23 +0200

    worked on my new version of website

--- new/homepage/script.js
+++ new/homepage/script.js
@@ -43,4 +43,46 @@ function updateLanguage(){
     else{
         console.error("dev fucked something up again, this time with changing language");
     }
+
+    updateSite();
+}
+
+function updateSite(){
+    var polishElements = document.getElementsByTagName("pl");
+    var englishElements = document.getElementsByTagName("eng");
+
+    //for debugging
+    //console.log(polishElements);
+    //console.log(englishElements);
+
+    if(language == "polish"){
+        for(let i = 0;  i < polishElements.length; i++){
+            polishElements[i].style.display = "block";
+        }
+        for(let i = 0; i < englishElements.length; i++){
+            englishElements[i].style.display = "none";
+        }
+    }
+    else if(language == "english"){
+        for(let i = 0; i < englishElements.length; i++){
+            englishElements[i].style.display = "block";    
+        }
+        for(let i = 0; i < polishElements.length; i++){
+            polishElements[i].style.display = "none";
+        }
+    }
+    else{
+        alert("there was an error while setting language!");
+    }
+}
+
+function allowCookies(){
+    document.getElementById("cookiesInfo").style.fontSize = "0";
+    window.setTimeout(function(){ 
+        document.getElementById("cookiesInfo").style.display = "none";
+    }, 240);
+}
+
+function exitWebsite(){
+    close();
 }
