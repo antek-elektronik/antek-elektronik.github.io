c8ab49516e27338e3ecd18f4a5e8888e61194154
Antek Gzara <antek.gzara@gmail.com>
2023-03-20 22:19:43 +0100

    added site about keyboards (in polish)

---
+++ klawiatury/script.js
@@ -0,0 +1,19 @@
+function mainpage(){
+    window.location = "index.html";
+}
+
+function producenci(){
+    window.location = "producenci.html";
+}
+
+function budowa(){
+    window.location = "budowa.html";
+}
+
+function rozmiary(){
+    window.location = "rozmiary.html";
+}
+
+function autor(){
+    window.location = "autor.html";
+}
